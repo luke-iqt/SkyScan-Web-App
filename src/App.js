ac19e1cbe6c384bf1bd04f400b461da2651a39d8
Luke Berndt <lberndt@iqt.org>
2021-02-18 09:36:58 -0500

    FPN model

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const threshold = 0.60;
 function App() {
   const [results, setResults] = useState([]);
   const [model, setModel] = useState(null);
-  const [modelURL, setModelURL] = useState('web_model3/model.json');
+  const [modelURL, setModelURL] = useState('web_mobilenet_fpn_model/model.json');
   const [webcam, setWebcam] = useState(null);
   const [showWebcam, setShowWebcam] = useState(false);
   const [objPred, setObjPred] = useState(null);
@@ -64,7 +64,7 @@ function App() {
   
     const devices = await navigator.mediaDevices.enumerateDevices();
     console.log(devices);
-    const webcamConfig = { resizeWidth: 300, resizeHeight: 300, centerCrop: false, facingMode: 'environment'}
+    const webcamConfig = { resizeWidth: 640, resizeHeight: 640, centerCrop: false, facingMode: 'environment'}
     const webcam = await tf.data.webcam(videoRef.current,webcamConfig);
     setWebcam(webcam);
     if (!modelReady) {
@@ -108,9 +108,17 @@ function App() {
     ctx.textBaseline = "top";
 
     //Getting predictions
-    const  classes = predictions[7].arraySync()
-    const scores = predictions[4].arraySync()
-    const boxes = predictions[6].arraySync()
+    const  classes = predictions[2].arraySync() //7
+    const scores = predictions[4].arraySync()  //4
+    const boxes = predictions[3].arraySync()  //6
+    /*const prediction0 = predictions[0].arraySync()
+    const prediction1 = predictions[1].arraySync()
+    const prediction2 = predictions[2].arraySync()
+    const prediction3 = predictions[3].arraySync()
+    const prediction4 = predictions[4].arraySync()
+    const prediction5 = predictions[5].arraySync()
+    const prediction6 = predictions[6].arraySync()
+    const prediction7 = predictions[7].arraySync()*/
     const detections = buildDetectedObjects(scores, threshold,
       boxes, classes, classList);
 
@@ -144,12 +152,12 @@ function App() {
 
   const detect = async () => {
     const img = await webcam.capture();
-    let tensor = img.reshape([1,300, 300,3]).toInt(); // change the image size
+    let tensor = img.reshape([1,640, 640,3]).toInt(); // change the image size
 
 
     let offset = tf.scalar(127.5);
-    var  new_frame = img.expandDims().reshape([1, 300, 300, 3]);    
-    var test_frame = tf.expandDims(img.toInt()).reshape([-1, 300, 300, 3]);
+    var  new_frame = img.expandDims().reshape([1, 640, 640, 3]);    
+    var test_frame = tf.expandDims(img.toInt()).reshape([-1, 640, 640, 3]);
 
     const predictions = await model.executeAsync(tensor); 
     renderPredictions(predictions)
@@ -196,12 +204,12 @@ function App() {
       )}
       {showWebcam && (
        <div id="video-box"  >  
-      <video autoPlay playsInline muted id="webcam" width="500px" height="500px" ref={videoRef} />
+      <video autoPlay playsInline muted id="webcam" width="640px" height="640px" ref={videoRef} />
       <canvas
           className="size"
           ref={canvasRef}
-          width="500px"
-          height="500px"
+          width="640px"
+          height="640px"
         />
       </div> 
       )}
